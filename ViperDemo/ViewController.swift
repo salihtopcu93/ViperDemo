c3ae6fd6ddb6c8a7fe7ccc2f2c1da924eca7bd27
salih topcu <salih.topcu@kgteknoloji.com>
2019-07-02 10:26:52 +0300

    Initial Commit

---
+++ ViperDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViperDemo
+//
+//  Created by salih topcu on 2.07.2019.
+//  Copyright © 2019 salih topcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
